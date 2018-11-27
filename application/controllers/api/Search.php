600ce8a279f4172bf5804bba8335e8aaeb9613ad
msmamh <msmamh@gmail.com>
2018-11-27 16:32:56 +0300

    Initial Build
    1- Setup CodeIgniter v3.1.9 MVC , PHP 7.x on wamp/lamp
    
    2- Optimize CodeIgniter and make it even faster and create special hooks
    
    2- Setup .htaccess [remove index.php from url]& config.php
    
    3- Integerate
    chriskacerguis/codeigniter-restserver with current version.
    
    3- Download Composer, Guzzle[Promises for http(s) requests), and Nahid/JsonQ[For Quering Json] and create libraries for this custom build of CodeIgniter.

---
+++ application/controllers/api/Search.php
@@ -0,0 +1,117 @@
+<?php
+error_reporting(E_ALL);
+
+
+use Restserver\Libraries\REST_Controller;
+use GuzzleHttp\Promise;
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+// This can be removed if you use __autoload() in config.php OR use Modular Extensions
+/** @noinspection PhpIncludeInspection */
+//To Solve File REST_Controller not found
+require APPPATH . 'libraries/REST_Controller.php';
+require APPPATH . 'libraries/Format.php';
+
+/**
+ *
+ * Search REST Class
+ *
+ * @package         CodeIgniter
+ * @subpackage      Rest Server
+ * @category        Controller
+ * @author          Base created by by Phil Sturgeon, Chris Kacerguis
+ * @license         MIT
+ * @link            https://github.com/chriskacerguis/codeigniter-restserver
+ */
+class Search extends REST_Controller {
+
+    protected $data = [];
+    protected $owner_temp = null;
+    function __construct()
+    {
+
+        // Construct the parent class
+        parent::__construct();
+        $this->load->library('guzzle');
+        $this->load->library('jsonq');
+        // Configure limits on our controller methods
+        // Ensure you have created the 'limits' table and enabled 'limits' within application/config/rest.php
+        $this->methods['users_get']['limit'] = 500; // 500 requests per hour per user/key
+        $this->methods['users_post']['limit'] = 100; // 100 requests per hour per user/key
+        $this->methods['users_delete']['limit'] = 50; // 50 requests per hour per user/key
+    }
+
+
+   
+
+    public function users_get()
+    {
+        // Users from a data store e.g. database
+        $users = [
+            ['id' => 1, 'name' => 'John', 'email' => 'john@example.com', 'fact' => 'Loves coding'],
+            ['id' => 2, 'name' => 'Jim', 'email' => 'jim@example.com', 'fact' => 'Developed on CodeIgniter'],
+            ['id' => 3, 'name' => 'Jane', 'email' => 'jane@example.com', 'fact' => 'Lives in the USA', ['hobbies' => ['guitar', 'cycling']]],
+        ];
+
+        $id = $this->get('id');
+
+        // If the id parameter doesn't exist return all the users
+
+        if ($id === NULL)
+        {
+            // Check if the users data store contains users (in case the database result returns NULL)
+            if ($users)
+            {
+                // Set the response and exit
+                $this->response($users, REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
+            }
+            else
+            {
+                // Set the response and exit
+                $this->response([
+                    'status' => FALSE,
+                    'message' => 'No users were found'
+                ], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+            }
+        }
+
+        // Find and return a single record for a particular user.
+
+        $id = (int) $id;
+
+        // Validate the id.
+        if ($id <= 0)
+        {
+            // Invalid id, set the response and exit.
+            $this->response(NULL, REST_Controller::HTTP_BAD_REQUEST); // BAD_REQUEST (400) being the HTTP response code
+        }
+
+        // Get the user from the array, using the id as key for retrieval.
+        // Usually a model is to be used for this.
+
+        $user = NULL;
+
+        if (!empty($users))
+        {
+            foreach ($users as $key => $value)
+            {
+                if (isset($value['id']) && $value['id'] === $id)
+                {
+                    $user = $value;
+                }
+            }
+        }
+
+        if (!empty($user))
+        {
+            $this->set_response($user, REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
+        }
+        else
+        {
+            $this->set_response([
+                'status' => FALSE,
+                'message' => 'User could not be found'
+            ], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+        }
+    }
+}
---
+++ application/libraries/Jsonq.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mohameed.Saeed
+ * Date: 11/26/2018
+ * Time: 11:26 AM
+ */
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Jsonq {
+    public function __construct()
+    {
+        // require_once('vendor/autoload.php');
+        require __DIR__ .'/jsonq/vendor/autoload.php';
+
+    }
+}
