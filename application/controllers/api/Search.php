b22f67671c4fa898ce49d2844b90a2bb439129f6
msmamh <msmamh@gmail.com>
2018-11-27 16:37:50 +0300

    Vendor GET REST API
    Created a vendor REST GET API in Search controller. This will call based on config vendor settings. check config.php. Current settings for GitHub.

--- application/controllers/api/Search.php
+++ application/controllers/api/Search.php
@@ -42,7 +42,101 @@ class Search extends REST_Controller {
     }
 
 
-   
+    public function vendor_get(){
+
+        $client = new \GuzzleHttp\Client;
+
+        //Get GET parameters
+        $per_page_value = (($this->get(config_item('per_page'))!=null && $this->get(config_item('per_page'))!='')?$this->get(config_item('per_page')):config_item('per_page_default'));
+        $per_page = ((config_item('per_page')!=null && config_item('per_page_default')!=null && config_item('per_page')!='' && config_item('per_page_default')!='')?config_item('per_page').'='.$per_page_value:null);
+
+        $page_value = (($this->get(config_item('page'))!=null && $this->get(config_item('page'))!='')?$this->get(config_item('page')):config_item('page_default'));
+        $page = ((config_item('page')!=null && config_item('page_default')!=null && config_item('page')!='' && config_item('page_default')!='')?config_item('page').'='.$page_value:null);
+
+        $query_value = (($this->get(config_item('query'))!=null && $this->get(config_item('query'))!='')?$this->get(config_item('query')):'');
+        $query = ((config_item('query')!=null && config_item('query')!='')?config_item('query').'='.$query_value:null);
+
+        $sort_value = (($this->get(config_item('sort'))!=null && $this->get(config_item('sort'))!='')?$this->get(config_item('sort')):config_item('sort_default'));
+        $sort = ((config_item('sort')!=null && config_item('sort_default')!=null && config_item('sort')!='' && config_item('sort_default')!='')?config_item('sort').'='.$sort_value:null);
+
+        $order_value = (($this->get(config_item('order'))!=null && $this->get(config_item('order'))!='')?$this->get(config_item('order')):config_item('order_default'));
+        $order = ((config_item('order')!=null && config_item('order_default')!=null && config_item('order')!='' && config_item('order_default')!='')?config_item('order').'='.$order_value:null);
+
+
+
+        //Building the url from config - or we can use http_build_query()
+
+       $url = config_item('search_code_url').'?'.
+            (($per_page!=null)?'&'.$per_page:'').
+            (($page!=null)?'&'.$page:'').
+            (($query!=null)?'&'.$query:'').
+            (($sort!=null)?'&'.$sort:'').
+            (($order!=null)?'&'.$order:'');
+
+        // Send an asynchronous request.
+        $request = new \GuzzleHttp\Psr7\Request('GET',$url,config_item('headers'));
+        $promise = $client->sendAsync($request)->then(function ($response) {
+            $this->data =  $response->getBody()->getContents();
+        });
+        $promise->wait(); // wait till promise fully executed.
+
+        $items = null;
+        $results = [
+          config_item('total_count') => 0,
+          config_item('per_page')  => $per_page_value,
+          config_item('page') => $page_value,
+          config_item('items') => null
+        ];
+
+        try {
+            //Getting Total Results.
+            $jq = new \Nahid\JsonQ\Jsonq(); // Magic JsonQ
+            $jq->json($this->data);
+            $total_count = $jq->from(config_item('total_count'))->get();
+            $results[config_item('total_count')] = $total_count;
+
+            $jq2 = new \Nahid\JsonQ\Jsonq(); // Magic JsonQ
+            $jq2->json($this->data);
+
+            if($sort_value != null && $order_value != null && config_item('deep_sort') === TRUE) {
+
+              $items = $jq2->from('items')->sortBy($sort_value, $order_value);
+              $results[config_item('items')] = $items;
+            }
+            else {
+
+
+                    $items = $jq2->from(config_item('items'))->transform(function ($arr){
+                    $client2 = new \GuzzleHttp\Client;
+                    // Send an asynchronous request.
+                    $request2 = new \GuzzleHttp\Psr7\Request('GET',$arr[config_item('repository')][config_item('owner')][config_item('owner_url')],config_item('headers'));
+                    $promise2 = $client2->sendAsync($request2)->then(function ($response) {
+                        $this->owner_temp = $response->getBody()->getContents();
+                    });
+                    $promise2->wait();
+                    $ownerjq = new \Nahid\JsonQ\Jsonq();
+                    $ownerjq->json($this->owner_temp);
+
+                    $owner = $ownerjq->from(config_item('owner_name'))->get();
+                    return [
+                        'owner_name'=> $owner,
+                        'repository_name' =>$arr[config_item('repository')][config_item('repo_name')],
+                        'file_name'=>$arr[config_item('filename')]
+                    ];
+                });
+              $results[config_item('items')] = $items;
+            }
+
+         // Deep sort
+         $this->response($results,REST_Controller::HTTP_OK);
+        } catch (\Nahid\JsonQ\Exceptions\ConditionNotAllowedException $e) {
+            echo $e->getMessage();
+        } catch (\Nahid\JsonQ\Exceptions\NullValueException $e) {
+            echo $e->getMessage();
+        }
+
+
+    }
 
     public function users_get()
     {
