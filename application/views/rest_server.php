f6abccbbc20c07c5403a63303fe7beff5f9ae860
msmamh <msmamh@gmail.com>
2018-11-30 13:16:30 +0300

    Updated Examples page.
    Updated links samples

--- application/views/rest_server.php
+++ application/views/rest_server.php
@@ -78,36 +78,32 @@
         <h2><a href="<?php echo site_url(); ?>">Home</a></h2>
 
         <p>
-            See the article
-            <a href="http://net.tutsplus.com/tutorials/php/working-with-restful-services-in-codeigniter-2/" target="_blank">
-                http://net.tutsplus.com/tutorials/php/working-with-restful-services-in-codeigniter-2/
-            </a>
-        </p>
-
-        <p>
             The master project repository is
-            <a href="https://github.com/chriskacerguis/codeigniter-restserver" target="_blank">
-                https://github.com/chriskacerguis/codeigniter-restserver
+            <a href="https://github.com/msmamh/erestapi" target="_blank">
+                https://github.com/msmamh/erestapi
             </a>
         </p>
 
         <p>
-            Click on the links to check whether the REST server is working.
+            Click on the links to check whether the REST server is working. Note that default vendor settings are done for GitHub.
+            You can always change JSON object properties - based on your vendor - from config.php
         </p>
 
         <ol>
-            <li><a href="<?php echo site_url('api/example/users'); ?>">Users</a> - defaulting to JSON</li>
-            <li><a href="<?php echo site_url('api/example/users/format/csv'); ?>">Users</a> - get it in CSV</li>
-            <li><a href="<?php echo site_url('api/example/users/id/1'); ?>">User #1</a> - defaulting to JSON  (users/id/1)</li>
-            <li><a href="<?php echo site_url('api/example/users/1'); ?>">User #1</a> - defaulting to JSON  (users/1)</li>
-            <li><a href="<?php echo site_url('api/example/users/id/1.xml'); ?>">User #1</a> - get it in XML (users/id/1.xml)</li>
-            <li><a href="<?php echo site_url('api/example/users/id/1/format/xml'); ?>">User #1</a> - get it in XML (users/id/1/format/xml)</li>
-            <li><a href="<?php echo site_url('api/example/users/id/1?format=xml'); ?>">User #1</a> - get it in XML (users/id/1?format=xml)</li>
-            <li><a href="<?php echo site_url('api/example/users/1.xml'); ?>">User #1</a> - get it in XML (users/1.xml)</li>
-            <li><a id="ajax" href="<?php echo site_url('api/example/users/format/json'); ?>">Users</a> - get it in JSON (AJAX request)</li>
-            <li><a href="<?php echo site_url('api/example/users.html'); ?>">Users</a> - get it in HTML (users.html)</li>
-            <li><a href="<?php echo site_url('api/example/users/format/html'); ?>">Users</a> - get it in HTML (users/format/html)</li>
-            <li><a href="<?php echo site_url('api/example/users?format=html'); ?>">Users</a> - get it in HTML (users?format=html)</li>
+
+            <li><a href="<?php echo site_url('api/search/vendor?q=php+in:file+language:php+repo:nahid/jsonq&page=1&order=asc'); ?>"> Vendor </a> - defaulting to JSON</li>
+            <li><a href="<?php echo site_url('api/search/vendor?q=php+in:file+language:php+repo:nahid/jsonq&page=1&order=asc&format=xml'); ?>"> Vendor </a> - get in XML</li>
+            <li><a href="<?php echo site_url('api/search/users'); ?>">Users</a> - defaulting to JSON</li>
+            <li><a href="<?php echo site_url('api/search/users/format/csv'); ?>">Users</a> - get it in CSV</li>
+            <li><a href="<?php echo site_url('api/search/users/id/1'); ?>">User #1</a> - defaulting to JSON  (users/id/1)</li>
+            <li><a href="<?php echo site_url('api/search/users/1'); ?>">User #1</a> - defaulting to JSON  (users/1)</li>
+            <li><a href="<?php echo site_url('api/search/users/id/1.xml'); ?>">User #1</a> - get it in XML (users/id/1.xml)</li>
+            <li><a href="<?php echo site_url('api/search/users/id/1/format/xml'); ?>">User #1</a> - get it in XML (users/id/1/format/xml)</li>
+            <li><a href="<?php echo site_url('api/search/users/id/1?format=xml'); ?>">User #1</a> - get it in XML (users/id/1?format=xml)</li>
+            <li><a href="<?php echo site_url('api/search/users/1.xml'); ?>">User #1</a> - get it in XML (users/1.xml)</li>
+            <li><a href="<?php echo site_url('api/search/users.html'); ?>">Users</a> - get it in HTML (users.html)</li>
+            <li><a href="<?php echo site_url('api/search/users/format/html'); ?>">Users</a> - get it in HTML (users/format/html)</li>
+            <li><a href="<?php echo site_url('api/search/users?format=html'); ?>">Users</a> - get it in HTML (users?format=html)</li>
         </ol>
 
     </div>
