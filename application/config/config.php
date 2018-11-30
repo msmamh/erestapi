fa8361e0172a64fd3c2a63c0bf2ffef68ac2c160
msmamh <msmamh@gmail.com>
2018-11-30 16:12:58 +0300

    Revert "Updating Config"
    This reverts commit ef58f6f088c6a1cf888f2a88cbaefda5aa5be033.

--- application/config/config.php
+++ application/config/config.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
+$ENVIRONMENT = 'production';
 /*
 |--------------------------------------------------------------------------
 | Base Site URL
