ef58f6f088c6a1cf888f2a88cbaefda5aa5be033
msmamh <msmamh@gmail.com>
2018-11-30 13:14:10 +0300

    Updating Config
    Move the project to production.

--- application/config/config.php
+++ application/config/config.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
+define('ENVIRONMENT', 'production');
 /*
 |--------------------------------------------------------------------------
 | Base Site URL
